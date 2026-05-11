0a5af2f2f4d8eee509aa28de2ac2c5311ecda438
M.amin <m.aminraadi@yahoo.com>
2026-05-11 13:09:48 +0330

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Play Game 2048 </title>
+        <meta name="description" content="Game 2048 Ceate React " />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Hello coder?</main>
+    </>
+  );
+}
